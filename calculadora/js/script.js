0c27e2a5569ecf493956dc84a405b84fe09a6eae
biscoito2202 <evelyn.lorkievicz@gmail.com>
2024-10-03 15:22:06 +0000

    Iniciado o código JavaScript

---
+++ calculadora/js/script.js
@@ -0,0 +1,14 @@
+// FAZ REFERENCIA AOS ELEMNTOS DOM
+const numero1 = document.querySelector('#numero1')
+const numero2 = document.querySelector('#numero2')
+const btnCalcular = document.querySelector('#btn-calcular')
+const resultado = document.querySelector('.resultado')
+const divisao = document.querySelector('.divisao')
+const adicao = document.querySelector('adicao')
+const subtracao = document.querySelector('subtracao')
+const multiplicacao = document.querySelector('multiplicaco')
+
+// alert(numero1)
+// CRIAR A FUNÇÃO 
+
+// ADICIONAR ESCUTAR DE EVENTOS
