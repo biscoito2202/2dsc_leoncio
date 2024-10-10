9c698788632e462d7d933bcb24a6ce4db99e2b60
biscoito2202 <evelyn.lorkievicz@gmail.com>
2024-10-10 15:24:26 +0000

    funçoes

--- calculadora/js/script.js
+++ calculadora/js/script.js
@@ -10,5 +10,21 @@ const multiplicacao = document.querySelector('multiplicaco')
 
 // alert(numero1)
 // CRIAR A FUNÇÃO 
+function calcular(){
+    const n1 = Number(numero1.value)
+    const n2 = Number(numero2.value)
+    if(typeof n1 === 'number' && typeof n2 === 'number') {
+        const adicao = `A soma de ${n1} e ${n2} = ${n1+n2}`
+        alert(adicao)
+    } else {
+        alert('Por favor, digite um número correto.')
+    }
+    //alert('Resultado :' + resultado)
+}
+    
+}
 
 // ADICIONAR ESCUTAR DE EVENTOS
+btnCalcular.addEventListener('click', function(){
+    calcular()
+})
